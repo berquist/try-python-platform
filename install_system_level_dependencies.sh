9be5eb75d4b27380f5d909e23e7880fd6d780ee1
Eric Berquist <eric.berquist@gmail.com>
2024-06-10 11:17:22 -0400

    add Arch Linux

--- install_system_level_dependencies.sh
+++ install_system_level_dependencies.sh
@@ -8,4 +8,6 @@ if command -v dnf >/dev/null 2>&1; then
 elif command -v apt-get >/dev/null 2>&1; then
     apt-get update -y
     apt-get install -y python3
+elif command -v pacman >/dev/null 2>&1; then
+    pacman -S --noconfirm python
 fi
