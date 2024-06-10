14f98153694252a4a423bbae8456e92862b9b058
Eric Berquist <eric.berquist@gmail.com>
2024-06-10 11:13:31 -0400

    fixes for installing Python from distro

--- install_system_level_dependencies.sh
+++ install_system_level_dependencies.sh
@@ -6,5 +6,6 @@ export DEBIAN_FRONTEND=noninteractive
 if command -v dnf >/dev/null 2>&1; then
     dnf install -y python3.12
 elif command -v apt-get >/dev/null 2>&1; then
+    apt-get update -y
     apt-get install -y python3
 fi
