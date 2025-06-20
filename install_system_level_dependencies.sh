55a154b1976cb64eaa78293d656c351a43506a6a
Eric Berquist <eric.berquist@gmail.com>
2025-06-20 18:12:49 -0400

    apt-get --no-install-recommends

--- install_system_level_dependencies.sh
+++ install_system_level_dependencies.sh
@@ -6,8 +6,8 @@ export DEBIAN_FRONTEND=noninteractive
 if command -v dnf >/dev/null 2>&1; then
     dnf install -y python3.12
 elif command -v apt-get >/dev/null 2>&1; then
-    apt-get update -y
-    apt-get install -y python3
+    apt-get update -y --no-install-recommends
+    apt-get install -y --no-install-recommends python3
 elif command -v pacman >/dev/null 2>&1; then
     pacman -Syu
     pacman -S --noconfirm python
